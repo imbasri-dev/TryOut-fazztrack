2edd309bb03b5c8487b242b185a51aaa10c0dd3e
imbasri.dev <imbasri.dev@gmail.com>
2022-08-08 15:29:27 +0700

    task 1 done

--- Level 1/task2.js
+++ Level 1/task2.js
@@ -1,2 +1,20 @@
+// task 2
+// 1.
 var nama = "Imam Basri";
 let umur = 25;
+const alamat = "Desa Sungai Kedukan , Palembang";
+let hobi = "game dan coding";
+let asalSekolah = "SMKN 4 Jakarta";
+let jurusan = "Teknik Audio Video";
+let alasanSukaProgramming =
+  "karena sangat mengasikkan ketika membuat sesuatu program dan berhasil untuk dipakai kemudian waktu";
+
+// 2.
+console.log(
+  `Nama saya adalah ${nama} , asal sekolah ${asalSekolah} jurusan ${jurusan}. Alasan saya tertarik dengan programming adalah ${alasanSukaProgramming}`
+);
+
+// 3.
+console.log(
+  "var : variabel ini scope ya sangatlah global dan data dalam variabel ini dapat dirubah dan var ini dapat dibuat kembali dengan nama yang sama dan isi yang berbeda. let : let adalah variabel yang dapat merubah data ya tetapi tidak dapat merubah nama inisial variabel yang sudah dibuatnya. const : variabel konstanta yang tidak dapat dirubah nama dan isi data ya"
+);
---
+++ Level 1/task3.js
@@ -0,0 +1,40 @@
+// task 3
+
+// 1
+// Ini comment satu baris
+
+/* tipe data dalam JavaScript : String = "berupa data huruf/karakter sekaligus angka", Integer = "Bilangan Bulat" , Float = "bilangan pecahan",Boolean = "Bilangan yang berisi data benar dan salah / true false", Object = "Data yang bisa menampung dari tipe data lainnya",  undefined = "data yang belum di inisialisasi , Null = "data sudah dibuat namun yang berisi kosong / null*/
+
+// 2
+// Tipe Data String
+let namaDepan = "Imam";
+let namaBelakang = "Basri";
+let alamat = "Desa Sungai Kedukan";
+let asalKelahiran = "Jakarta";
+const jenisKelamin = "Laki-laki";
+// Tipe Data Number
+let umur = 25;
+let noHP = 085280029990;
+const tanggalLahir = 12081996;
+
+// 3
+
+const angkaPertama = 2000;
+const angkaKedua = 22;
+let hasil;
+
+// operator tambah/penjumlahan
+hasil = angkaPertama + angkaKedua;
+console.log(hasil);
+
+// operator kurang
+hasil = angkaPertama - angkaKedua;
+console.log(hasil);
+
+// operator bagi
+hasil = angkaPertama / angkaKedua;
+console.log(hasil);
+
+// operator kali
+hasil = angkaPertama * angkaKedua;
+console.log(hasil);
